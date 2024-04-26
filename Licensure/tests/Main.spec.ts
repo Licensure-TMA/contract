85c6ded9f7ae8422bbe12b13afcd12faaa957744
Aleksandr Glebov <89081349143@mail.ru>
2024-04-26 15:39:26 -0600

    contract v1

--- Licensure/tests/Main.spec.ts
+++ Licensure/tests/Main.spec.ts
@@ -38,4 +38,204 @@ describe('Main', () => {
         // the check is done inside beforeEach
         // blockchain and main are ready to use
     });
+
+    it('should create', async () => {
+        const arrayBefore = await main.getArrayOfLicenses()
+        console.log("arrayBefore:", arrayBefore.length)
+        console.log("arrayBefore:", arrayBefore.map)
+        const createdAt = new Date();
+        const dateString = createdAt.toISOString();
+
+        await main.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.05')
+            },
+            {
+                $$type: 'LicenseCreate',
+                licenseId: 1234567n,
+                sellerAddress: deployer.address,
+                buyerAddress: deployer.address,
+                createdAt: dateString,
+                contentName: 'Videos with cats',
+                contentDescription: "super",
+                contentUrls:"1 - https://docs.tact-lang.org/cookbook/data-structures",
+                licenseType: "Restricted license",
+                contentCategory: "Video",
+                contentSubcategory: "Internet video",
+                price: 50n,
+                currency: "TON",
+                allRestrictions: "Duration: 1 year; Purpose: Training neural networks, Marketing; Modification: No",
+                additionalTerms: "",
+                status: "Pending"
+            }
+        )
+
+        await main.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.05')
+            },
+            {
+                $$type: 'LicenseCreate',
+                licenseId: 87654n,
+                sellerAddress: deployer.address,
+                buyerAddress: deployer.address,
+                createdAt: dateString,
+                contentName: 'Videos with dogs',
+                contentDescription: "super",
+                contentUrls:"1 - https://docs.tact-lang.org/cookbook/data-structures",
+                licenseType: "Restricted license",
+                contentCategory: "Video",
+                contentSubcategory: "Internet video",
+                price: 50n,
+                currency: "TON",
+                allRestrictions: "Duration: 1 year; Purpose: Training neural networks, Marketing; Modification: No",
+                additionalTerms: "",
+                status: "Pending"
+            }
+        )
+
+        const arrayAfter = await main.getArrayOfLicenses()
+        console.log("arrayAfter:", arrayAfter.length)
+        console.log("arrayAfter:", arrayAfter.map)
+
+        expect(arrayBefore.length).toBeLessThan(arrayAfter.length)
+    });
+
+    it('should get', async () => {
+        const createdAt = new Date();
+        const dateString = createdAt.toISOString();
+
+        await main.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.05')
+            },
+            {
+                $$type: 'LicenseCreate',
+                licenseId: 1234567n,
+                sellerAddress: deployer.address,
+                buyerAddress: deployer.address,
+                createdAt: dateString,
+                contentName: 'Videos with cats',
+                contentDescription: "super",
+                contentUrls:"1 - https://docs.tact-lang.org/cookbook/data-structures",
+                licenseType: "Restricted license",
+                contentCategory: "Video",
+                contentSubcategory: "Internet video",
+                price: 50n,
+                currency: "TON",
+                allRestrictions: "Duration: 1 year; Purpose: Training neural networks, Marketing; Modification: No",
+                additionalTerms: "",
+                status: "Pending"
+            }
+        )
+
+        const license = await main.getLicense(1234567n)
+        console.log(license)
+
+        expect(license != null)
+    });
+
+    it('should delete', async () => {
+        const createdAt = new Date();
+        const dateString = createdAt.toISOString();
+
+        await main.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.05')
+            },
+            {
+                $$type: 'LicenseCreate',
+                licenseId: 1234567n,
+                sellerAddress: deployer.address,
+                buyerAddress: deployer.address,
+                createdAt: dateString,
+                contentName: 'Videos with cats',
+                contentDescription: "super",
+                contentUrls:"1 - https://docs.tact-lang.org/cookbook/data-structures",
+                licenseType: "Restricted license",
+                contentCategory: "Video",
+                contentSubcategory: "Internet video",
+                price: 50n,
+                currency: "TON",
+                allRestrictions: "Duration: 1 year; Purpose: Training neural networks, Marketing; Modification: No",
+                additionalTerms: "",
+                status: "Pending"
+            }
+        )
+
+        await main.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.05')
+            },
+            {
+                $$type: 'LicenseCreate',
+                licenseId: 12324567n,
+                sellerAddress: deployer.address,
+                buyerAddress: deployer.address,
+                createdAt: dateString,
+                contentName: 'Videos with dogs',
+                contentDescription: "super",
+                contentUrls:"1 - https://docs.tact-lang.org/cookbook/data-structures",
+                licenseType: "Restricted license",
+                contentCategory: "Video",
+                contentSubcategory: "Internet video",
+                price: 50n,
+                currency: "TON",
+                allRestrictions: "Duration: 1 year; Purpose: Training neural networks, Marketing; Modification: No",
+                additionalTerms: "",
+                status: "Pending"
+            }
+        )
+
+        await main.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.05')
+            },
+            {
+                $$type: 'LicenseCreate',
+                licenseId: 12134567n,
+                sellerAddress: deployer.address,
+                buyerAddress: deployer.address,
+                createdAt: dateString,
+                contentName: 'Videos with apples',
+                contentDescription: "super",
+                contentUrls:"1 - https://docs.tact-lang.org/cookbook/data-structures",
+                licenseType: "Restricted license",
+                contentCategory: "Video",
+                contentSubcategory: "Internet video",
+                price: 50n,
+                currency: "TON",
+                allRestrictions: "Duration: 1 year; Purpose: Training neural networks, Marketing; Modification: No",
+                additionalTerms: "",
+                status: "Pending"
+            }
+        )
+
+        const arrayBefore = await main.getArrayOfLicenses()
+        console.log("arrayBefore:", arrayBefore.length)
+        console.log("arrayBefore:", arrayBefore.map)
+
+        await main.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.05')
+            },
+            {
+                $$type: 'LicenseDelete',
+                licenseId: 12324567n
+            }
+        )
+
+        const arrayAfter = await main.getArrayOfLicenses()
+        console.log("arrayAfter:", arrayAfter.length)
+        console.log("arrayAfter:", arrayAfter.map)
+
+        expect(arrayAfter.length).toBeLessThan(arrayBefore.length)
+    });
 });
